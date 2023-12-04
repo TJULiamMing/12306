f698d128102d18117afbfa227970fdca02866b86
liam <1292766268@qq.com>
2023-12-04 23:57:26 +0800

    修改启动日志，增加/member前缀，修改logback日志

--- member/src/main/java/train/member/config/MemberApplication.java
+++ member/src/main/java/train/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("train.member")
 public class MemberApplication {
+    private static final Logger LOG= LoggerFactory.getLogger(MemberApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址:\thttp://127.0.0.1:{}/hello",env.getProperty("server.port"));
     }
-
 }
